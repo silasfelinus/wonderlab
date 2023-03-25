72ebff64c9c1b77553cea4caf47b664f5c85c18f
silasfelinus <silasfelinus@gmail.com>
2023-03-24 23:41:55 -0700

    building chatwidget

--- src/components/ChatWidget.vue
+++ src/components/ChatWidget.vue
@@ -1,125 +1,99 @@
 <template>
-    <div class="chat-window" :style="{backgroundColor: backgroundColor, color: textColor}">
-      <div class="chatroom-selection">
-        <label for="chatroom">Choose a chatroom:</label>
-        <select id="chatroom" v-model="selectedChatroom">
-          <option v-for="chatroom in chatrooms" :key="chatroom.id" :value="chatroom.id">{{ chatroom.name }}</option>
-        </select>
+  <div class="chatwidget">
+    <div class="messages">
+      <div v-for="(message, index) in messages" :key="index" class="message">
+        {{ message }}
       </div>
-      <div class="messages">
-        <p v-for="msg in messages" :key="msg.id">{{ msg.text }}</p>
-      </div>
-      <div class="input-container">
-        <input type="text" v-model="inputMessage" @keyup.enter="sendMessage" placeholder="Type your message here..." />
-        <button @click="sendMessage">Send</button>
-      </div>
-      <button class="hidden-button" @click="hiddenAction">Hidden Button</button>
     </div>
-  </template>
+    <div class="input-container">
+      <input
+        type="text"
+        v-model="inputMessage"
+        @keyup.enter="submitMessage"
+        placeholder="Type your message..."
+      />
+      <button @click="submitMessage">Send</button>
+    </div>
+  </div>
+</template>
 
 <script>
 export default {
+  name: "ChatWidget",
   data() {
     return {
-      chatrooms: [
-        { id: 1, name: 'ChatGPT' },
-        { id: 2, name: 'Stable Diffusion' },
-        { id: 3, name: 'Acrocat Ranch' },
-        { id: 3, name: 'Cafe Fred' },
-      ],
-      selectedChatroom: 1,
-      messages: [
-        { id: 1, text: 'Hello, welcome to the chat!' },
-        { id: 2, text: 'This is a sample message.' },
-      ],
-      inputMessage: '',
-      backgroundColor: '#ffffff',
-      textColor: '#000000',
+      messages: [],
+      inputMessage: "",
     };
   },
   methods: {
-    sendMessage() {
-      if (this.inputMessage.trim() !== '') {
-        const newMessage = {
-          id: this.messages.length + 1,
-          text: this.inputMessage,
-        };
-        this.messages.push(newMessage);
-        this.inputMessage = '';
+    submitMessage() {
+      if (this.inputMessage.trim()) {
+        this.messages.push(this.inputMessage.trim());
+        this.inputMessage = "";
       }
-    },
-    hiddenAction() {
-      // Add your hidden action here
-      console.log('Hidden button clicked!');
     },
   },
 };
 </script>
 
 <style scoped>
-.chat-window {
-  width: 500px;
-  height: 300px;
-  padding: 20px;
-  border: 1px solid #cccccc;
-  border-radius: 5px;
+.chatbox {
+  display: flex;
+  flex-direction: column
+
+
+
+
+
+<style scoped>
+.chatbox {
   display: flex;
   flex-direction: column;
-  gap: 10px;
-}
-
-.chatroom-selection {
-  display: flex;
-  align-items: center;
+  justify-content: space-between;
+  width: 400px;
+  height: 600px;
+  border: 2px solid #333;
+  border-radius: 10px;
+  padding: 20px;
+  background-color: #f0f0f0;
+  font-family: Arial, sans-serif;
 }
 
 .messages {
-  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
   overflow-y: auto;
-  border: 1px solid #cccccc;
+  height: 80%;
+  margin-bottom: 20px;
+}
+
+.message {
   padding: 10px;
+  background-color: #ddd;
   border-radius: 5px;
+  margin-bottom: 10px;
 }
 
 .input-container {
   display: flex;
-  gap: 5px;
+  align-items: center;
 }
 
-input[type='text'] {
+input[type="text"] {
   flex-grow: 1;
-  padding: 5px;
-  border: 1px solid #cccccc;
+  padding: 10px;
   border-radius: 5px;
+  border: 1px solid #999;
 }
 
 button {
-  padding: 5px 10px;
-  border: 1px solid #cccccc;
-  background-color: #ffffff;
-  color: #000000;
-  cursor: pointer;
+  padding: 10px;
+  margin-left: 10px;
+  border: none;
   border-radius: 5px;
-}
-
-button:hover {
-  background-color: #cccccc;
-}
-
-.hidden-button {
-  position: absolute;
-  top: 0;
-  right: 0;
-  opacity: 0;
-  width: 50px;
-  height: 50px;
-}
-
-.hidden-button:hover {
-  opacity: 0.3;
-}
-
-.hidden-button:focus {
-  opacity: 0.5;
+  background-color: #333;
+  color: #fff;
+  cursor: pointer;
 }
 </style>
