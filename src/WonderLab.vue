c924d2b777aa142eaf9bb4fec7fffb9d04a88152
silasfelinus <silasfelinus@gmail.com>
2023-03-25 17:34:07 -0700

    trying to launch server with resize

--- src/WonderLab.vue
+++ src/WonderLab.vue
@@ -1,23 +1,37 @@
 <template>
   <div id="wonderlab">
-    <vue-drag-resize>
-      <widget-shed />
-    </vue-drag-resize>
+    <VueDragResize :isActive="true" :w="200" :h="200" v-on:resizing="resize" v-on:dragging="resize">
+            <h3>Hello World!</h3>
+            <p>{{ top }} х {{ left }} </p>
+            <p>{{ width }} х {{ height }}</p>
+        </VueDragResize>
   </div>
 </template>
 
 <script>
-import WidgetShed from './components/WidgetShed.vue';
+    import VueDragResize from 'vue-drag-resize';
 
-export default {
-  components: {
-    WidgetShed
+    export default {
+      data() {
+    return {
+      width: 0,
+      height: 0,
+      top: 0,
+      left: 0,
+    };
+  },
+  methods: {
+    resize(newRect) {
+      this.width = newRect.width;
+      this.height = newRect.height;
+      this.top = newRect.top;
+      this.left = newRect.left;
+    },
   },
 };
 </script>
 
 <style scoped>
-/* Add your CSS styles here */
 #wonderlab {
   display: flex;
   justify-content: center;
