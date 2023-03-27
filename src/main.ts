321fcda35a918d9768fbc515566b7a57f3d37d0e
silasfelinus <silasfelinus@gmail.com>
2023-03-26 20:00:48 -0700

    bug hunting

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ import store from './store';
 import './styles/main.scss';
 import { Quasar } from 'quasar';
 import 'quasar/dist/quasar.css';
+import quasarUserOptions from './quasar-user-options';
 
 const app = createApp(App);
 app.use(store);
-app.use(Quasar);
+app.use(Quasar, quasarUserOptions);
 app.mount('#app');
--- src/quasar-user-options.ts
+++ src/quasar-user-options.ts
@@ -1,8 +1,20 @@
-import { QuasarPluginOptions } from 'quasar';
+import type { QuasarLanguage, QuasarIconSet } from 'quasar';
 
 const quasarUserOptions: QuasarPluginOptions = {
-  config: {},
+  lang: QuasarLanguage['en-us'],
+  iconSet: QuasarIconSet['material-icons'],
+  components: {},
+  directives: {},
   plugins: {},
+  screen: {
+    xs: 0,
+    sm: 600,
+    md: 1024,
+    lg: 1440,
+    xl: 1920,
+  },
+  autoImportComponentCase: 'kebab',
+  autoImportDirectiveCase: 'kebab',
 };
 
 export default quasarUserOptions;
