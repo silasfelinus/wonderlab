d1bb12e25eabd0b97cd725e7abd9289ec18d88a7
silasfelinus <silasfelinus@gmail.com>
2023-03-26 18:17:03 -0700

    fixed chatwidget

--- src/components/ChatWidget.vue
+++ src/components/ChatWidget.vue
@@ -1,41 +1,49 @@
 <template>
-    <q-drawer side="left">
-      <q-chat-message
-        v-for="(message, index) in messages"
-        :key="index"
-        :sent="message.sent"
-        :text="message.text"
-      />
-      <q-input
-        v-model="messageInput"
-        outlined
-        @keyup.enter="sendMessage"
-        placeholder="Type your message..."
-      />
-    </q-drawer>
-  </template>
-  
-  <script lang="ts">
-  import { defineComponent, ref } from 'vue';
-  
-  export default defineComponent({
-    setup() {
-      const messageInput = ref('');
-      const messages = ref([]);
-  
-      function sendMessage() {
-        if (messageInput.value) {
-          messages.value.push({ sent: true, text: messageInput.value });
-          messageInput.value = '';
-        }
+  <q-drawer side="left">
+    <q-list>
+      <q-item v-for="(message, index) in messages" :key="index">
+        <q-chat-message
+          :sent="message.sent"
+          :text="message.text"
+          :bg-color="message.sent ? 'primary' : 'grey-3'"
+          :text-color="message.sent ? 'white' : 'dark'"
+        />
+      </q-item>
+    </q-list>
+    <q-input
+      v-model="messageInput"
+      outlined
+      @keyup.enter="sendMessage"
+      placeholder="Type your message..."
+    />
+  </q-drawer>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, Ref } from 'vue';
+
+interface Message {
+  sent: boolean;
+  text: string;
+}
+
+export default defineComponent({
+  setup() {
+    const messageInput = ref('');
+    const messages: Ref<Message[]> = ref([]);
+
+    function sendMessage() {
+      if (messageInput.value) {
+        messages.value.push({ sent: true, text: messageInput.value });
+        messageInput.value = '';
       }
-  
-      return {
-        messageInput,
-        messages,
-        sendMessage,
-      };
-    },
-  });
-  </script>
-  
+    }
+
+    return {
+      messageInput,
+      messages,
+      sendMessage,
+    };
+  },
+});
+</script>
---
+++ src/quasar-user-options.ts
@@ -0,0 +1,8 @@
+import { QuasarPluginOptions } from 'quasar';
+
+const quasarUserOptions: QuasarPluginOptions = {
+  config: {},
+  plugins: {},
+};
+
+export default quasarUserOptions;
